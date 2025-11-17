744d61aa1ddebb7920624693830d39dd886b8b45
BartlomiejWrobel95 <bartlomiejwrobel95@users.noreply.github.com>
2025-11-17 17:40:39 +0100

    memory usage (free vs used including percentage)

--- server-stats.sh
+++ server-stats.sh
@@ -6,5 +6,17 @@
 # grab  -------------------------------^ (idle percentage) and substract from 100
 cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
 cpu_usage=$(echo "100 - $cpu_idle" | bc)
-echo "Cpu Usage: $cpu_usage %"
+echo "Cpu Usage: $cpu_usage%"
+
 ######################################
+# Total memory usage
+# Grab total, used and free memory values
+memory_total=$(free -m | grep Mem: | awk '{print $2}')  
+mem_used=$(free -m | grep Mem: | awk '{print $3}')
+mem_free=$(free -m | grep Mem: | awk '{print $4}')
+# Calculate memory usage and free percentages
+mem_usage_percent=$(echo "100 * $mem_used / $memory_total" | bc)
+mem_free_percent=$(echo "100 * $mem_free / $memory_total" | bc)
+# Output memory usage
+echo "Memory Used: $mem_used MB($mem_usage_percent%)"
+echo "Memory Free: $mem_free MB($mem_free_percent%)"
